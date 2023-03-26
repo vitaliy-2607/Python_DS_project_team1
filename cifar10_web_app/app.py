38a0fb5d3d5105fbc27b092fc14827a856637bdc
Roman-Braneshty <102828727+Roman-Braneshty@users.noreply.github.com>
2023-03-26 19:40:24 +0300

    Redirect

--- cifar10_web_app/app.py
+++ cifar10_web_app/app.py
@@ -1,21 +1,18 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html', os=os)
-
 
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.files['file']
     filename = file.filename
     file.save(os.path.join('static', 'uploads', filename))
-    return 'Image successfully uploaded!'
-
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
